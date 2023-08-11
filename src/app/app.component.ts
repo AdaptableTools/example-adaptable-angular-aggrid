705b4dc2acc12ab8d4790e697000eaf4695cb704
Andrei Floricel <andrei.floricel@gmail.com>
2023-08-11 12:52:04 +0300

    update to latest versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component } from '@angular/core';
-import { ColDef, GridOptions, Module } from '@ag-grid-community/core';
-import {
-  AdaptableApi,
-  AdaptableButton,
-  AdaptableOptions, AdaptableReadyInfo,
-  CustomToolbarButtonContext,
-  CustomToolPanelButtonContext,
-  ToolPanelButtonContext,
-} from '@adaptabletools/adaptable-angular-aggrid';
-import finance from '@adaptabletools/adaptable-plugin-finance';
-import { ButtonToggleComponent } from './custom-components/button-toggle.component';
-import { SlideToggleComponent } from './custom-components/slide-toggle.component';
-import { MaterialMenuComponent } from './custom-components/material-menu.component';
+import {Component} from '@angular/core';
+import {ColDef, GridOptions, Module} from '@ag-grid-community/core';
+import {AdaptableApi, AdaptableOptions, AdaptableReadyInfo,} from '@adaptabletools/adaptable-angular-aggrid';
 import {ITrade, rowData} from './rowData';
-import { ThemeSettingsPanelComponent } from './custom-components/theme-settings-panel.component';
-import { RECOMMENDED_MODULES } from '../agGridModules';
+import {RECOMMENDED_MODULES} from '../agGridModules';
 
 @Component({
   selector: 'app-adaptable-root',
@@ -59,320 +47,70 @@ export class AppComponent {
 
   private isLayoutShortcutMenuDisabled = true;
 
-  public adaptableOptions: AdaptableOptions<ITrade> = {
-    primaryKey: 'tradeId',
-    userName: 'demo user',
-    // licenseKey: <add_provided_license_key>,
-    adaptableId: 'AdapTable Angular Demo',
-    plugins: [finance()],
-    settingsPanelOptions: {
-      customSettingsPanels: [
-        {
-          name: 'Custom Theme Settings Panel',
-          frameworkComponent: {
-            // The custom component wraps the same reusable Angular toggle component which is used in the Toolbar and ToolPanel components
-            type: ThemeSettingsPanelComponent,
-          },
-        },
-      ],
+  public adaptableOptions: AdaptableOptions<ITrade> =  {
+  // @ts-ignore
+    primaryKey: '',
+    autogeneratePrimaryKey: true,
+    layoutOptions: {
+      autoSaveLayouts: false
     },
-    dashboardOptions: {
-      customToolbars: [
-        {
-          name: 'GithubRepo',
-          title: 'Github Repo',
-          showConfigureButton: false,
-          toolbarButtons: [
-            {
-              label: 'See Source Code',
-              buttonStyle: {
-                variant: 'raised',
-                tone: 'info',
-              },
-              icon: {
-                src:
-                  'https://www.pngkey.com/png/full/178-1787243_github-icon-png-github-icon-white-png.png',
-                style: {
-                  width: 24,
-                  height: 24,
-                },
-              },
-              onClick: () => {
-                (window as any)
-                  ?.open(
-                    'https://github.com/AdaptableTools/example-adaptable-angular-aggrid',
-                    '_blank'
-                  )
-                  .focus();
-              },
-            },
-          ],
-        },
-        // Show a Title and Configure Button
-        {
-          name: 'CustomThemeToolbar',
-          title: 'Custom Theme Toolbar',
-          showConfigureButton: false,
-          toolbarButtons: [
-            {
-              label: 'Open Theme Settings',
-              buttonStyle: {
-                variant: 'raised',
-                tone: 'accent',
-              },
-              onClick: (
-                button: AdaptableButton<CustomToolbarButtonContext>,
-                context: CustomToolbarButtonContext
-              ) => {
-                context.adaptableApi.settingsPanelApi.openCustomSettingsPanel(
-                  'Custom Theme Settings Panel'
-                );
-              },
-            },
-          ],
-        },
-        {
-          name: 'LayoutToggle',
-          title: 'Layout toggle',
-          frameworkComponent: {
-            // simple wrapper around Angular Material ButtonToggle component
-            // the implementation (and interaction with the AdaptableApi) is encapsulated in the component
-            type: ButtonToggleComponent,
-          },
-        },
-        {
-          name: 'SlideToggle',
-          title: 'Slide toggle',
-          frameworkComponent: {
-            // custom Angular component for a slide component
-            // additional configuration is passed through the onSetup() function
-            type: SlideToggleComponent,
-            onSetup: (): Partial<SlideToggleComponent> => {
-              return {
-                // basically this toolbar interacts with another custom toolbar(layoutMenu), activating/deactivating it
-                onChange: toggleValue => {
-                  this.isLayoutShortcutMenuDisabled = !toggleValue;
-                },
-              };
-            },
-          },
-        },
-        {
-          name: 'LayoutMenu',
-          title: 'Layout menu',
-          frameworkComponent: {
-            // custom component providing an Angular Material menu in a custom toolbar menu :)
-            // the implementation is generic, the I/O params are set through the onSetup() function
-            type: MaterialMenuComponent,
-            onSetup: ({ adaptableApi }): Partial<MaterialMenuComponent> => {
-              return {
-                // the component interacts with the adaptableApi provided in the framework component params
-                menuItems: adaptableApi.layoutApi
-                  .getLayouts()
-                  .map(layout => layout.Name),
-                onItemClick: layoutName => {
-                  adaptableApi.layoutApi.setLayout(layoutName);
-                },
-                // the disabled state is updated by another custom toolbar (slideToggle)
-                isDisabled: () => {
-                  return this.isLayoutShortcutMenuDisabled;
-                },
-              };
-            },
-          },
-        },
-      ],
+    userInterfaceOptions: {
+      showDocumentationLinks: false
     },
-    toolPanelOptions: {
-      customToolPanels: [
-        {
-          name: 'SlideToggle',
-          title: 'Slide toggle',
-          frameworkComponent: {
-            // custom Angular component for a slide component
-            // additional configuration is passed through the onSetup() function
-            type: SlideToggleComponent,
-            onSetup: (): Partial<SlideToggleComponent> => {
-              return {
-                // basically this ToolPanel interacts with another custom ToolPanel(layoutMenu), activating/deactivating it
-                onChange: toggleValue => {
-                  this.isLayoutShortcutMenuDisabled = !toggleValue;
-                },
-              };
-            },
-          },
-        },
-        {
-          name: 'LayoutMenu',
-          title: 'Layout menu',
-          frameworkComponent: {
-            // custom component providing an Angular Material menu in a custom ToolPanel menu
-            // the implementation is generic, the I/O params are set through the onSetup() function
-            type: MaterialMenuComponent,
-            onSetup: ({ adaptableApi }): Partial<MaterialMenuComponent> => {
-              return {
-                // the component interacts with the adaptableApi provided in the framework component params
-                menuItems: adaptableApi.layoutApi
-                  .getLayouts()
-                  .map(layout => layout.Name),
-                onItemClick: layoutName => {
-                  adaptableApi.layoutApi.setLayout(layoutName);
-                },
-                // the disabled state is updated by another custom ToolPanel (slideToggle)
-                isDisabled: () => {
-                  return this.isLayoutShortcutMenuDisabled;
-                },
-              };
-            },
-          },
-        },
-        {
-          name: 'CustomToolPanelButton',
-          buttons: [
-            {
-              label: 'AlertButton',
-              buttonStyle: {
-                variant: 'raised',
-                tone: 'accent',
-              },
-              onClick: (
-                button: AdaptableButton<CustomToolPanelButtonContext>,
-                context: CustomToolPanelButtonContext
-              ) => {
-                context.adaptableApi.alertApi.showAlertInfo(
-                  'CustomToolPanelButton',
-                  'Styled button & icon'
-                );
-              },
-            },
-          ],
-        },
-      ],
-      customButtons: [
-        {
-          label: 'Query Popup',
-          icon: {
-            src: 'https://img.icons8.com/glyph-neue/64/000000/zoom-in.png',
-          },
-          buttonStyle: {
-            variant: 'outlined',
-            // tone: 'accent',
-          },
-          onClick: (
-            button: AdaptableButton<ToolPanelButtonContext>,
-            context: ToolPanelButtonContext
-          ) => {
-            context.adaptableApi.queryApi.openQuerySettingsPanel();
-          },
-        },
-      ],
+    gridOptions: {
+      sideBar: ['filters', 'columns']
     },
-
-    predefinedConfig: {
-      Dashboard: {
-        Revision: Date.now(),
-        ModuleButtons: ['SettingsPanel'],
-        Tabs: [
+    stateOptions: {
+      loadState: () => Promise.resolve(),
+      saveState: () => {},
+      persistState: () => Promise.resolve()
+    },
+    predefinedConfig:{
+      FreeTextColumn: {
+        FreeTextColumns: [
           {
-            Name: 'Custom',
-            Toolbars: [
-              'GithubRepo',
-              'CustomThemeToolbar',
-              'LayoutToggle',
-              'SlideToggle',
-              'LayoutMenu',
+            ColumnId: 'comments',
+            FriendlyName: 'Comments',
+            FreeTextStoredValues: [
+              {PrimaryKey: 24195339, FreeText: 'Used by US team'},
+              {PrimaryKey: 224663696, FreeText: 'My favourite'},
+              {PrimaryKey: 82095231, FreeText: 'Required by Support'},
             ],
+            FreeTextColumnSettings: {
+              Resizable: true,
+              DataType: 'String',
+            },
           },
           {
-            Name: 'Search',
-            Toolbars: ['Query'],
-          },
-        ],
-      },
-      Layout: {
-        CurrentLayout: 'Basic',
-        Layouts: [
-          {
-            Name: 'Basic',
-            Columns: [
-              'tradeId',
-              'currency',
-              'history',
-              'changeOnYear',
-              'counterparty',
-              'bid',
-              'bidOfferSpread',
-              'ask',
-              'notional',
-              'status',
-              'statusActionColumn',
-              'country',
-              'price',
-              'isLive',
-              'rating',
-              'tradeDate',
-              'settlementDate',
-            ],
+            ColumnId: 'orderCode',
+            FriendlyName: 'Order Code',
+            DefaultValue: 123,
+            FreeTextColumnSettings: {
+              DataType: 'Number',
+              Sortable: false,
+              SuppressMenu: true,
+            },
           },
           {
-            Name: 'Sorted',
-            Columns: [
-              'tradeId',
-              'currency',
-              'changeOnYear',
-              'counterparty',
-              'history',
-              'settlementDate',
+            ColumnId: 'isUsed',
+            FriendlyName: 'Is Used',
+            DefaultValue: false,
+            FreeTextStoredValues: [
+              {PrimaryKey: 10270250, FreeText: true},
+              {PrimaryKey: 24195339, FreeText: true},
+              {PrimaryKey: 224663696, FreeText: true},
+              {PrimaryKey: 82095231, FreeText: true},
             ],
-            ColumnSorts: [
-              {
-                ColumnId: 'currency',
-                SortOrder: 'Asc',
-              },
-              {
-                ColumnId: 'counterparty',
-                SortOrder: 'Desc',
-              },
-            ],
-          },
-          {
-            Name: 'Row Grouped',
-            Columns: [
-              'tradeId',
-              'changeOnYear',
-              'counterparty',
-              'bid',
-              'bidOfferSpread',
-              'ask',
-              'notional',
-              'status',
-            ],
-            RowGroupedColumns: ['country', 'currency'],
-          },
-          {
-            Name: 'Pivot',
-            Columns: [],
-            PivotColumns: ['status'],
-            RowGroupedColumns: ['counterparty'],
-            EnablePivot: true,
-            AggregationColumns: {
-              bid: 'avg',
-              ask: 'sum',
-              price: true,
+            FreeTextColumnSettings: {
+              DataType: 'Boolean',
+              Filterable: true,
+              Groupable: true,
             },
           },
         ],
       },
-      CustomSort: {
-        CustomSorts: [
-          {
-            ColumnId: 'currency',
-            SortedValues: ['USD', 'GBP', 'EUR'],
-          },
-        ],
-      },
-    },
-  };
+    }
+  }
 
   constructor() {
     this.columnDefs = [
